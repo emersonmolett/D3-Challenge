3f0435f7f3746d59c6cfdcaa0dc1b311aa05b263
Emerson Molett <emerson.molett@gmail.com>
2021-04-29 21:24:56 -0500

    starting to create circiles

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -24,12 +24,12 @@ var chartGroup = svg.append("g")
     .attr("transform", `translates(${margin.left}, ${margin.top})`);
 
 // get data from csv file
-d3.csv("assets/data/data.csv").then(function(healthData, err) {
+d3.csv("assets/data/data.csv").then(function (healthData, err) {
     if (err) throw err;
     // console.log(healthData);
 
     // parse data
-    healthData.forEach(function(data) {
+    healthData.forEach(function (data) {
         data.smoke = +data.smoke;
         data.poverty = +data.poverty;
         data.age = +data.age;
@@ -47,8 +47,17 @@ var xLinearScale = d3.scaleLinear()
 var yLinearScale = d3.scaleLinear()
     .domain([9, d3.max(healthData, d => d.smokes) * 2.9])
     .range([height, 0]);
-    console.log(d3.extent(healthData, d => d.smokes));
+console.log(d3.extent(healthData, d => d.smokes));
 
 // axis functions 
 var bottomAxis = d3.axisBottom(xLinearScale);
 var leftAxis = d3.axisLeft(yLinearScale);
+
+// append axes to chart
+var xAxis = chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+chartGroup.append("g")
+    .call(leftAxis);
