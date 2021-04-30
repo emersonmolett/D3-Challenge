43040609937e045bddf9049fb39a665c284b9c0d
Emerson Molett <emerson.molett@gmail.com>
2021-04-29 19:30:10 -0500

    starting to work on js code

--- Instructions/StarterCode/assets/js/app.js
+++ Instructions/StarterCode/assets/js/app.js
@@ -1,1 +1,13 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 700;
+var svgHeight = 500;
+
+var margin = {
+    top:20, 
+    right: 40, 
+    bottom: 80, 
+    left: 100,
+};
+
+var width = svgWidth - margin.left - margin.right;
+var heiht = svgHeight - margin.top - margin.bottom; 
